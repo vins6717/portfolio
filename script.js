530ee1538166675fea2e5c97be32b8e16062d579
Shannon Gurule <shannon.gurule@gmail.com>
2022-07-24 15:19:29 -0400

    remove js

--- script.js
+++
@@ -1,7 +0,0 @@
-const navLink = document.querySelectorAll('.nav-link');
-
-navLink.forEach(link => {
-    link.addEventListener('scroll', () => {
-        link.parentNode.classList.toggle('active')
-    })
-})
